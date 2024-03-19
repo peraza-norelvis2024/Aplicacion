4db4209f32769287c4fb2e0860a90756c08e1bfe
Norelvis Peraza <perazanorelvis@gmail.com>
2024-03-18 22:16:12 -0400

    CRUD de Semestre y otros

--- src/controlador/CPeriodoAcademicoAdmin.java
+++ src/controlador/CPeriodoAcademicoAdmin.java
@@ -1,7 +1,33 @@
 // Norelvis Peraza // Joe Verde // Sophia Estrada // Juan Fonseca // Gloria Sánchez
 
 package controlador;
+import vistas.PeriodoAcademicoAdmin;
+import vistas.DashboardAdministrador;
+
+import modelo.Sesion;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.*;
+import java.util.ArrayList;
+import javax.swing.*;
+
+import conexion.CConexion;
+import java.util.*;
+import modelo.MPeriodoAcademico;
 
 public class CPeriodoAcademicoAdmin {
+    private PeriodoAcademicoAdmin view;
+    private Sesion sesion;
+    private CConexion cconexion = new CConexion();
+    private HashMap<String, Integer> mapaPeriodoAcademico; 
+    private MPeriodoAcademico periodoAcademico;
+
+    Connection connection = null;
+    PreparedStatement statement = null;
+    ResultSet resultSet = null;
     
+    public CPeriodoAcademicoAdmin(PeriodoAcademicoAdmin view, Sesion sesion){
+        
+    }
 }
